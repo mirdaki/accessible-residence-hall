bbf53eec5a2b4951509b6ed47c75426953a901b6
mirdaki <mirdaki@users.noreply.github.com>
2018-12-10 17:32:42 -0500

    Fix event listeners not being disposed of properly
    - Moved change-scene-on-click code to init from update to have the event listener only be created once
    - Renamed change-scene-on-click component to be called scene in order to refrence itself
        - JavaScript can't refrence a hypen in a name
        - This may be caused by an A-Frame issue not letting the component refrence itself from another function

--- components/change-scene-on-click.js
+++ components/change-scene-on-click.js
@@ -1,28 +1,34 @@
-AFRAME.registerComponent('change-scene-on-click', {
+AFRAME.registerComponent('scene', {
     schema: {
-        color: {default: 'green'},
         sceneNum: {default: 0}
     },
 
-    update: function () {
-        var data = this.data;
-        var el = this.el;  // <a-box>
-        var defaultColor = el.getAttribute('material').color;
-        var sky = document.querySelector('a-sky');
-        
-        function loadPorts(transports) {
-            var boxes = document.querySelectorAll('.model');
-            for (var i=0; i< boxes.length; i++){
-                //I think we can reuse the same two/three box port objects and just set their data appropriately.
-                //As long as we have exactly the same number of boxes in every scene, we need no additional logic.
-                boxes[i].setAttribute('position', transports[i].pos);
-                boxes[i].setAttribute('change-scene-on-click', 
-                    "sceneNum: " + transports[i].sceneIndex);
-            }
-            return;
+    init: function () {      
+        this.el.addEventListener('click', clickFunction);
+
+        function clickFunction() {            
+            var data = this.components.scene.data;
+            var sky = document.querySelector('a-sky');
+    
+            var scene = scenelist[data.sceneNum];
+            sky.setAttribute('src', scene.sceneref);
+            sky.setAttribute('phi-start', scene.sceneyrot);
+            //Set sky yaw.
+            loadPois(scene.tooltips, data);
+            loadPorts(scene.transports);
+            startNarration(scene.narration);
         }
-        
-        function loadPois(tooltips){
+
+        function startNarration(narration){
+            var narrScene = document.querySelector('#sceneNarration');
+            narrScene.setAttribute('sound', {
+                'src': narration
+            });
+            narrScene.components.sound.playSound();
+            console.log("THIS IS NARRATION " + narration);
+        }
+    
+        function loadPois(tooltips, data){
             var circles = document.querySelectorAll('a-circle');
             var scene = scenelist[data.sceneNum];
             var pois = document.querySelectorAll('.vidpanel');
@@ -41,43 +47,17 @@ AFRAME.registerComponent('change-scene-on-click', {
                     src: tooltips[i].poiNarration
                 });
             }
-            return;
         }
-
-        function startNarration(narration){
-            var narrScene = document.querySelector('#sceneNarration');
-            narrScene.setAttribute('sound', {
-                src: narration
-            });
-            narrScene.components.sound.playSound();
-            console.log("THIS IS NARRATION " + narration);
-            return;
+    
+        function loadPorts(transports) {
+            var boxes = document.querySelectorAll('.model');
+            for (var i=0; i< boxes.length; i++){
+                //I think we can reuse the same two/three box port objects and just set their data appropriately.
+                //As long as we have exactly the same number of boxes in every scene, we need no additional logic.
+                boxes[i].setAttribute('position', transports[i].pos);
+                boxes[i].setAttribute('scene', 
+                    "sceneNum: " + transports[i].sceneIndex);
+            }
         }
-
-        function clickFunction(){
-            //el.removeAttribute('click');
-            //el.setAttribute('change-scene-on-click', data);
-            el.setAttribute('color', defaultColor);
-            var scene = scenelist[data.sceneNum];
-            sky.setAttribute('src', scene.sceneref);
-            sky.setAttribute('phi-start', scene.sceneyrot);
-            el.setAttribute('color', defaultColor);
-            //Set sky yaw.
-            loadPois(scene.tooltips);
-            loadPorts(scene.transports);
-            startNarration(scene.narration);
-            el.removeEventListener('click', clickFunction);
-            return;
-        }
-
-        el.addEventListener('click', clickFunction);
-
-        el.addEventListener('mouseenter', function () {
-            el.setAttribute('color', data.color);
-        });
-
-        el.addEventListener('mouseleave', function () {
-            el.setAttribute('color', defaultColor);
-        });
     }
 });
